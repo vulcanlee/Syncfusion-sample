8ef42e40bfbda2bd9e222adb1ecd62b1e8883eff
Vulcan <vulcan.lee@gmail.com>
2021-01-20 16:22:51 +0800

    HtmlToPdfConverter

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+using Syncfusion.Pdf;
+using Syncfusion.HtmlConverter;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        public HomeController(ILogger<HomeController> logger,
+            IWebHostEnvironment webHostEnvironment)
+        {
+            _logger = logger;
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+
+        public IActionResult ExportToPDF()
+        {
+            //Initialize HTML to PDF converter 
+            HtmlToPdfConverter htmlConverter = new HtmlToPdfConverter(HtmlRenderingEngine.WebKit);
+
+            WebKitConverterSettings settings = new WebKitConverterSettings();
+
+            //Set WebKit path
+            settings.WebKitPath = Path.Combine(WebHostEnvironment.ContentRootPath, "QtBinariesWindows");
+
+            //Assign WebKit settings to HTML converter
+            htmlConverter.ConverterSettings = settings;
+
+            //Convert URL to PDF
+            PdfDocument document = htmlConverter.Convert("https://www.google.com");
+
+            //Saving the PDF to the MemoryStream
+            MemoryStream stream = new MemoryStream();
+
+            document.Save(stream);
+
+            //Download the PDF document in the browser
+            return File(stream.ToArray(), System.Net.Mime.MediaTypeNames.Application.Pdf, "Output.pdf");
+        }
+    }
+}
